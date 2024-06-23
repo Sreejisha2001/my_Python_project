ad8fc75429d446a0e30cf97db5aded7397d56cf6
Sreejisha <sreejishaj2001@gmail.com>
2024-06-23 12:49:09 +0530

    added description,images amd links and arranged columns in neet

--- main.py
+++ main.py
@@ -18,14 +18,21 @@ content=""" Below you can find some of the apps
 i have built in python. Feel free to contact me. You can find my contact details on this following link i have given here. Thank you !!!"""
 st.write(content)
 
-col3,col4=st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df=pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[source code]({row['url']})") #can add github link of each project
+
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source code]({row['url']})") #can add github link of each project
 
