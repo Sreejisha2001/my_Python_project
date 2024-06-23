ae76d52fea15bd69d6b98752e610416c37e4afaf
Sreejisha <sreejishaj2001@gmail.com>
2024-06-23 13:37:31 +0530

    send email

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.header("Contact Us")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Your Email Address")
+    message = st.text_area("Your message here")
+    button = st.form_submit_button()
+    if button:
+        message = message+user_email
