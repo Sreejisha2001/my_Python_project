6cc291f9f163edef62c711f43f5c6c79aff3dd6d
Sreejisha <sreejishaj2001@gmail.com>
2024-06-23 12:00:48 +0530

    added titles of projects in two columns

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import pandas
 import streamlit as st
+# to read csv files
+
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -14,3 +17,15 @@ with col2:
 content=""" Below you can find some of the apps 
 i have built in python. Feel free to contact me. You can find my contact details on this following link i have given here. Thank you !!!"""
 st.write(content)
+
+col3,col4=st.columns(2)
+
+df=pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
