e08babac0c4115057729289cb2a0ee6e524eb916
Sreejisha <sreejishaj2001@gmail.com>
2024-06-28 13:22:35 +0530

    add selectbox to choose purpose

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -6,12 +6,14 @@ st.header("Contact Us")
 with st.form(key="email_forms"):
     user_email = st.text_input("Your Email Address")
     # raw_message is without subject and other email format
+    option = st.selectbox(label="Purpose", options=("Doubt", "complaint", "feedback", "Discuss"),)
     raw_message = st.text_area("Your message here")
     # message variable has everything which is in email format
     message = f"""\
 Subject: New email from {user_email}
 
 from: {user_email}
+Purpose: {option}
 {raw_message}
 """
     button = st.form_submit_button("submit")
